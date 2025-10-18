d717a6246c2ccdcfcbb507d84663dd983cd849c4
Max Heinrich <max.heinrich.de@gmail.com>
2025-10-18 23:37:19 +0200

    broken state

--- internal/request/request.go
+++ internal/request/request.go
@@ -3,12 +3,25 @@ package request
 import (
 	"errors"
 	"io"
+	"log"
 	"strings"
 	"unicode"
 )
 
+var SEPERATOR = "\r\n"
+
+type parserState int
+
+const (
+	StateInit parserState = 0
+	StateDone parserState = 1
+)
+
+const BUFFERSIZE = 1028
+
 type Request struct {
 	RequestLine RequestLine
+	state       parserState
 }
 
 type RequestLine struct {
@@ -18,42 +31,92 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+	buf := make([]byte, 0, BUFFERSIZE)
+	// localBufferSize := BUFFERSIZE
+	bufLen := 0
+	req := NewRequest()
+	for req.state != StateDone {
+		// if len(buf) == localBufferSize {
+		// 	localBufferSize = 2 * localBufferSize
+		// 	newBuf := make([]byte, len(buf), localBufferSize)
+		// 	copy(nwBuf, buf)
+		// 	buf = newBuf
+		// }
+		n, err := reader.Read(buf[bufLen:])
+		if err == io.EOF {
+			req.state = StateDone
+			break // End of file, break the loop
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		bufLen += n
+
+		parsed, err := req.parse(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		copy(buf, buf[parsed:bufLen])
+		bufLen -= parsed
+
 	}
-	req := strings.Split(string(data), "\r\n")
-	reqLine, err := parseRequestLine(req[0])
-	if err != nil {
-		return nil, err
-	}
-	return &Request{
-		RequestLine: *reqLine,
-	}, nil
+	return req, nil
 }
 
-func parseRequestLine(line string) (*RequestLine, error) {
+func NewRequest() *Request {
+	return &Request{
+		state: StateInit,
+	}
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	if r.state == StateDone {
+		return 0, errors.New("trying to parse read stuff")
+	}
+	req, read, err := parseRequestLine(string(data))
+	if err != nil {
+		return 0, err
+	}
+
+	if read == 0 {
+		return 0, nil
+	}
+
+	r.state = StateDone
+	r.RequestLine = *req
+	return read, nil
+}
+
+func parseRequestLine(req string) (*RequestLine, int, error) {
+	idx := strings.Index(req, SEPERATOR)
+	if idx == -1 {
+		return nil, 0, nil
+	}
+
+	line := req[:idx]
+
 	parts := strings.Split(line, " ")
 	if len(parts) != 3 {
-		return &RequestLine{}, errors.New("Number of Arguments in Requenst Line not 3")
+		return &RequestLine{}, 0, errors.New("Number of Arguments in Requenst Line not 3")
 	}
 
 	// Method Part
 	for _, c := range parts[0] {
 		if unicode.IsLower(c) || !unicode.IsLetter(c) {
-			return &RequestLine{}, errors.New("Method Wrong")
+			return &RequestLine{}, 0, errors.New("Method Wrong")
 		}
 	}
 
 	// Version Part
 	version, found := strings.CutPrefix(parts[2], "HTTP/")
 	if !found || version != "1.1" {
-		return &RequestLine{}, errors.New("Version Wrong")
+		return nil, idx, errors.New("Version Wrong")
 	}
 
 	return &RequestLine{
 		HttpVersion:   version,
 		RequestTarget: parts[1],
 		Method:        parts[0],
-	}, nil
+	}, idx, nil
 }
